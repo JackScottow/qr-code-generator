dbe343d3b5a0880cc33bfac6c28a4b8580b6f85c
JackScottow <101928816+JackScottow@users.noreply.github.com>
2023-08-10 19:22:59 +0100

    Update qrcodegenerator.tsx

--- src/app/components/qrcodegenerator.tsx
+++ src/app/components/qrcodegenerator.tsx
@@ -17,7 +17,7 @@ const QRCodeGenerator: React.FC = () => {
   return (
     <div className="border-slate-200 border rounded-md flex flex-col lg:flex-row text-center  bg-slate-200 text-slate-900 p-4">
       <div className="border bg-slate-200 my-auto " id="qr-code">
-        <QrCode value={qrCodeValue} className="p-2 mx-auto bg-white" size={qrCodeSize} title={qrCodeValue} />
+        <QrCode value={qrCodeValue} className="p-2 mx-auto bg-white" size={qrCodeSize} />
         <p className="text-center ">{qrCodeValue}</p>
       </div>
 
@@ -27,15 +27,13 @@ const QRCodeGenerator: React.FC = () => {
           <input type="text" name="content" value={qrCodeValue} onChange={(e) => setQRCodeValue(e.target.value)} placeholder={qrCodeValue} />
         </div>
         <div className="flex flex-col my-2">
-          <label htmlfor="size">Size:</label>
-          <select name="size" className="text-center my-2" onChange={(e) => setQrCodeSize(e.target.value)}>
-            <option value={128}>128px x 128px</option>
-            <option value={256} selected="selected">
-              256px x 256px (Default)
-            </option>
+          <label htmlFor="size">Size:</label>
+          <select name="size" className="text-center my-2" onChange={(e) => setQrCodeSize(Number(e.target.value))}>
+            <option value="256">256px x 256px (Default)</option>
+            <option value="128">128px x 128px</option>
           </select>
         </div>
-        <button className="bg-transparent hover:bg-slate-900 text-slate-900 font-semibold hover:text-slate-200 py-2 my-2 px-4 border border-slate-900 hover:border-transparent rounded" onClick={(e) => handleDownload()}>
+        <button className="bg-transparent hover:bg-slate-900 text-slate-900 font-semibold hover:text-slate-200 py-2 my-2 px-4 border border-slate-900 hover:border-transparent rounded" onClick={() => handleDownload()}>
           Download
         </button>
       </div>
